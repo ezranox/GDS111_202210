6415ee989ffff11d0eb788e719d49be5c5176743
Ezra Rivera <sarivera@email.neit.edu>
2022-05-20 18:59:11 -0400

    Mechanic2

--- gds137/Mygame/js/basic_platformer.js
+++ gds137/Mygame/js/basic_platformer.js
@@ -6,23 +6,24 @@ var timer;
 var interval;
 var snake;
 var size = 15;
-
-
+var goal = [2];
+var smolFood = [20];
 
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
 
-	
-	
 	snake = new GameObject({x:100, y:canvas.height/2-100,color:"purple"});
 	
-	
 
-	
-
-		
-	goal = new GameObject({width:50, height:50, x:canvas.width-50, y:100, color:"pink"});
-	
+	//Constructor Functions for object arrays :)
+	for(i = 0;i < 2; i++)
+	{
+		goal[i] = new GameObject({width:50, height:50, x:Math.floor(Math.random() * (canvas.width -100) + 50), y:Math.floor(Math.random() * (canvas.height - 100) + 50), color:"pink"});
+	}
+	for(i = 0; i < 20;i++)
+	{
+		smolFood[i] = new GameObject({width:25, height:25, x:Math.floor(Math.random() * (canvas.width - 50) + 25), y:Math.floor(Math.random() * (canvas.height - 50) + 25), color:"aqua"})
+	}
 
 	var fX = .85;
 	var fY = .97;
@@ -65,24 +66,37 @@ function animate()
 	snake.x += Math.round(snake.vx);
 	snake.y += Math.round(snake.vy);
 	
-	
-	
-	
-	
-		
-	if(snake.hitTestObject(goal))
+
+	//Collision Detection for loops 
+
+	for(i = 0;i < 2;i++)
 	{
-		goal.y = 10000;
-		snake.height+=size;
-		snake.width+=size;
+	if(snake.hitTestObject(goal[i]))
+		{
+			goal[i].y = 10000;
+			snake.height+=size;
+			snake.width+=size;
+		}
+	}
+	for(i = 0; i < 20;i++)
+	{
+		if(snake.hitTestObject(smolFood[i]))
+		{
+			smolFood[i].y = 10000;
+			snake.height+=size/2;
+			snake.width+=size/2;
+		}
 	}
 	
-	
-	
-	
-
-	//Show hit points
+	//In order to draw each object in the array (as seperate pieces) 
 	snake.drawCircle();
-	goal.drawCircle();
+	for(i = 0; i < 20;i++)
+	{
+		smolFood[i].drawCircle();
+	}
+	for(i=0; i<2;i++)
+	{
+		goal[i].drawCircle();
+	}
 }
 
